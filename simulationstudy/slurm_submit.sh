59d5e85e312de6d386ab8a4cf4cb1c9ef6675902
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-06-14 15:48:45 +1200

    Create slurm_submit.sh

---
+++ simulationstudy/slurm_submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pspline_simulation_study
+#SBATCH --output=logs/download_%A_%a.log
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=2GB
+#SBATCH --cpus-per-task=1
+#SBATCH --array=0-5
+
+
+
+ml gcc/11.3.0 openmpi/4.1.4 python/3.10.4 r/4.2.1
+source /fred/oz303/naimen/bnpc/lisa_venv/activate
+python simulation.py $SLURM_ARRAY_TASK_ID 
+
+
+
+
+
