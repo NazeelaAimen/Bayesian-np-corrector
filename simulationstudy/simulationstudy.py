16090d417742868b2b0195a0d5fdc37f6b8da748
Nazeela Aimen <100678418+NazeelaAimen@users.noreply.github.com>
2024-06-19 00:53:36 +1200

    Add files via upload

--- simulationstudy/simulationstudy.py
+++ simulationstudy/simulationstudy.py
@@ -11,7 +11,7 @@ import rpy2.robjects as robjects
 import pickle
 import time
 import argparse
-
+import pandas as pd
 ##Random seed:
 parser = argparse.ArgumentParser(description='Simulation script with random seed.')
 parser.add_argument('random_seed', type=int, help='Random seed for the simulation')
@@ -24,12 +24,8 @@ robjects.r['source']('psd_arma.R')
 psd_arma = robjects.globalenv['psd_arma']
 mcmcr=importr("psplinePsd")
 
-rd = 2  
+
 #AR(4) coefficients
-a1=0.9
-a2=-0.9
-a3=0.9
-a4=-0.9
 sig=1
 #AR(1) coefficients
 a1p=0.1883
@@ -48,7 +44,7 @@ spar=[]
 for i in range(0,len(n)):
     random.seed(rd)
     #time series AR(4)
-    series.append({f'{n[i]}': bnpc.ar4(n[i], a1, a2, a3, a4, sig)})
+    series.append({f'{n[i]}': pd.read_csv(f"ts{n[i]}.csv").iloc[rd]})
     #frequency[0,pi]
     freq.append({f'{n[i]}': np.pi*( 2 * (np.arange(1, n[i] // 2 + 1) - 1) / n[i])[1:]})
     f_r=robjects.FloatVector(freq[i][f'{n[i]}'])
@@ -63,13 +59,31 @@ degree=3
 iterations=100000
 burnin=50000
 result_r=[]
+resultMH=[]
+resultMH_t=[]#result when the parametric model is true psd while updating lambda using MH
 result=[]
-result_t=[]#result when the parametric model is true psd
+result_t=[]#result when the parametric model is true psd while updating lambda using AMH
 r_t=[]
 p_t=[]
+pMH_t=[]
 p_t_t=[]
+pMH_t_t=[]
+
+
+####Calculating IAE and prop
+iae=[]
+iae_t=[]
+iaeMH=[]
+iaeMH_t=[]
+iae_r=[]
+prop=[]
+prop_t=[]
+propMH=[]
+propMH_t=[]
+prop_r=[]
 for i in range(0,len(n)): 
-    y_c=bnpc.cent_series(series[i][f'{n[i]}'])
+    #MCMC
+    y_c=np.array(bnpc.cent_series(series[i][f'{n[i]}']))
     with np_cv_rules.context():
         s_t_r = time.time()
         result_r.append( mcmcr.gibbs_pspline(
@@ -81,66 +95,79 @@ for i in range(0,len(n)):
                     k=k,
                     printIter =50000
                 ))
-        e_t_r = time.time()
-        r_t.append(e_t_r-s_t_r)
-        pdgrm=result_r[i]['pdgrm'][1:-1]
-        # AR(1)
-        stpy=time.time()
-        result.append(bnpc.mcmcAMH(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=spar[i][f'{n[i]}'], modelnum=1,f=freq[i][f'{n[i]}']))
-        etpy=time.time()
-        p_t.append(etpy-stpy)
-        # AR(4) 
-        stpy_t=time.time()
-        result_t.append(bnpc.mcmcAMH(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=np.exp(truepsd[i][f'{n[i]}']-2*np.log(np.std(series[i][f'{n[i]}']))), modelnum=1,f=freq[i][f'{n[i]}']))
-        etpy_t=time.time()
-        p_t_t.append(etpy_t-stpy_t)
-
-
-
-
-####Calculating IAE and prop
-iae=[]
-iae_t=[]
-iae_r=[]
-prop=[]
-prop_t=[]
-prop_r=[]
-for i in range(0,len(n)):
+    e_t_r = time.time()
+    r_t.append(e_t_r-s_t_r)
+    pdgrm=result_r[i]['pdgrm'][1:-1]
+    # AR(1)
+    ##AMH
+    stpy=time.time()
+    result.append(bnpc.mcmcAMH(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=spar[i][f'{n[i]}'], modelnum=1,f=freq[i][f'{n[i]}']))
+    etpy=time.time()
+    p_t.append(etpy-stpy)
+    #MH
+    stpy=time.time()
+    resultMH.append(bnpc.mcmc(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=spar[i][f'{n[i]}'], modelnum=1,f=freq[i][f'{n[i]}']))
+    etpy=time.time()
+    pMH_t.append(etpy-stpy)
+    # AR(4) 
+    #AMH
+    stpy_t=time.time()
+    result_t.append(bnpc.mcmcAMH(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=np.exp(truepsd[i][f'{n[i]}']-2*np.log(np.std(series[i][f'{n[i]}']))), modelnum=1,f=freq[i][f'{n[i]}']))
+    etpy_t=time.time()
+    p_t_t.append(etpy_t-stpy_t)
+    # MH
+    stpy_t=time.time()
+    resultMH_t.append(bnpc.mcmc(pdgrm=pdgrm, n=iterations, k=k, burnin=burnin, Spar=np.exp(truepsd[i][f'{n[i]}']-2*np.log(np.std(series[i][f'{n[i]}']))), modelnum=1,f=freq[i][f'{n[i]}']))
+    etpy_t=time.time()
+    pMH_t_t.append(etpy_t-stpy_t)
+    
+    #CI
     ci_py = bnpc.compute_ci(result[i]['psd']+2*np.log(np.std(series[i][f'{n[i]}'])))
     ci_py_t = bnpc.compute_ci(result_t[i]['psd']+2*np.log(np.std(series[i][f'{n[i]}'])))
+    ci_pyMH = bnpc.compute_ci(resultMH[i]['psd']+2*np.log(np.std(series[i][f'{n[i]}'])))
+    ci_py_tMH = bnpc.compute_ci(resultMH_t[i]['psd']+2*np.log(np.std(series[i][f'{n[i]}'])))
     ci_r = bnpc.compute_ci((np.log(result_r[i]['fpsd.sample'] * np.std(series[i][f'{n[i]}']) ** 2)[1:-1]).T)
+    #iae
     iae.append(bnpc.compute_iae(np.exp(ci_py.med),np.exp(truepsd[i][f'{n[i]}']),n[i]))
     iae_t.append(bnpc.compute_iae(np.exp(ci_py_t.med),np.exp(truepsd[i][f'{n[i]}']),n[i]))
+    iaeMH.append(bnpc.compute_iae(np.exp(ci_pyMH.med),np.exp(truepsd[i][f'{n[i]}']),n[i]))
+    iaeMH_t.append(bnpc.compute_iae(np.exp(ci_py_tMH.med),np.exp(truepsd[i][f'{n[i]}']),n[i]))
     iae_r.append(bnpc.compute_iae(np.exp(ci_r.med),np.exp(truepsd[i][f'{n[i]}']),n[i]))
+    #prop
     prop.append(bnpc.compute_prop(ci_py.u05,ci_py.u95,truepsd[i][f'{n[i]}']))        
     prop_t.append(bnpc.compute_prop(ci_py_t.u05,ci_py_t.u95,truepsd[i][f'{n[i]}']))
+    propMH.append(bnpc.compute_prop(ci_pyMH.u05,ci_pyMH.u95,truepsd[i][f'{n[i]}']))        
+    propMH_t.append(bnpc.compute_prop(ci_py_tMH.u05,ci_py_tMH.u95,truepsd[i][f'{n[i]}']))
     prop_r.append(bnpc.compute_prop(ci_r.u05,ci_r.u95,truepsd[i][f'{n[i]}']))
-
-
     
-
+    
 # import os
 # os.makedirs(f'sim_res_{n}')
 # 
 
 #Outputs:     
-col_names = 'py_AR(1)_iae_128 py_AR(1)_iae_256 py_AR(1)_iae_512 py_AR(4)_iae_128 py_AR(4)_iae_256 py_AR(4)_iae_512 r_iae_128 r_iae_256 r_iae_512'
-np.savetxt(f'sim_res/{rd}iae.txt', [iae,iae_t,iae_r], header=col_names)
+col_names = 'pyAMH_AR(1)_iae_128 pyAMH_AR(1)_iae_256 pyAMH_AR(1)_iae_512 pyAMH_AR(4)_iae_128 pyAMH_AR(4)_iae_256 pyAMH_AR(4)_iae_512 pyMH_AR(1)_iae_128 pyMH_AR(1)_iae_256 pyMH_AR(1)_iae_512 pyMH_AR(4)_iae_128 pyMH_AR(4)_iae_256 pyMH_AR(4)_iae_512 r_iae_128 r_iae_256 r_iae_512'
+np.savetxt(f'sim_res/{rd}iae.txt', [iae,iae_t,iaeMH,iaeMH_t,iae_r], header=col_names)
 
-col_names = 'py_AR(1)_prop_128 py_AR(1)_prop_256 py_AR(1)_prop_512 py_AR(4)_prop_128 py_AR(4)_prop_256 py_AR(4)_prop_512 r_prop_128 r_prop_256 r_prop_512'
-np.savetxt(f'sim_res/{rd}prop.txt', [prop,prop_t,prop_r], header=col_names)
+col_names = 'pyAMH_AR(1)_prop_128 pyAMH_AR(1)_prop_256 pyAMH_AR(1)_prop_512 pyAMH_AR(4)_prop_128 pyAMH_AR(4)_prop_256 pyAMH_AR(4)_prop_512 pyAMH_AR(1)_prop_128 pyAMH_AR(1)_prop_256 pyAMH_AR(1)_prop_512 pyAMH_AR(4)_prop_128 pyAMH_AR(4)_prop_256 pyAMH_AR(4)_prop_512 r_prop_128 r_prop_256 r_prop_512'
+np.savetxt(f'sim_res/{rd}prop.txt', [prop,prop_t,propMH,propMH_t,prop_r], header=col_names)
 
-col_names = 'py_AR(1)_run_t_128 py_AR(1)_run_t_256 py_AR(1)_run_t_512 py_AR(4)_run_t_128 py_AR(4)_run_t_256 py_AR(4)_run_t_512 r_tun_t_128 r_run_t_256 r_run_t_512'
-np.savetxt(f'sim_res/{rd}runtime.txt',  [p_t,p_t_t,r_t], header=col_names)
+col_names = 'pyAMH_AR(1)_run_t_128 pyAMH_AR(1)_run_t_256 pyAMH_AR(1)_run_t_512 pyAMH_AR(4)_run_t_128 pyAMH_AR(4)_run_t_256 pyAMH_AR(4)_run_t_512 pyMH_AR(1)_run_t_128 pyMH_AR(1)_run_t_256 pyMH_AR(1)_run_t_512 pyMH_AR(4)_run_t_128 pyMH_AR(4)_run_t_256 pyMH_AR(4)_run_t_512 r_tun_t_128 r_run_t_256 r_run_t_512'
+np.savetxt(f'sim_res/{rd}runtime.txt',  [p_t,p_t_t,pMH_t,pMH_t_t,r_t], header=col_names)
 
-with open(f'sim_res/{rd}resultpy_AR(1)_128_256_512.pkl', 'wb') as f:
+with open(f'sim_res/{rd}resultpyAMH_AR(1)_128_256_512.pkl', 'wb') as f:
     pickle.dump(result, f)
 
-with open(f'sim_res/{rd}resultpy_AR(4)_128_256_512.pkl', 'wb') as f:
+with open(f'sim_res/{rd}resultpyAMH_AR(4)_128_256_512.pkl', 'wb') as f:
     pickle.dump(result_t, f)
+
+with open(f'sim_res/{rd}resultpyMH_AR(1)_128_256_512.pkl', 'wb') as f:
+    pickle.dump(resultMH, f)
+
+with open(f'sim_res/{rd}resultpyMH_AR(4)_128_256_512.pkl', 'wb') as f:
+    pickle.dump(resultMH_t, f)
 
 with open(f'sim_res/{rd}resultr_128_256_512.pkl', 'wb') as f:
     pickle.dump(result_r, f)
 
-with open(f'sim_res/{rd}tsereis.pkl', 'wb') as f:
-    pickle.dump(series, f)
+
